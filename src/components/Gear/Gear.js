8bab928a4fecba8fdd1ba46f4104af39e1f1f688
Tata Rozhdez <tanya.zhukova.95@gmail.com>
2020-08-30 22:31:28 +0300

    finish adaptiv, start adding animation

--- src/components/Gear/Gear.js
+++ src/components/Gear/Gear.js
@@ -2,10 +2,12 @@ import React from 'react'
 import './Gear.css'
 
 export const Gear = () => (
-  <div className='gear-img'>
-    <div className='bar' />
-    <div className='bar' />
-    <div className='bar' />
-    <div className='gear-inner' />
+  <div className='gear grid-square block'>
+    <div className='gear-img'>
+      <div className='bar' />
+      <div className='bar' />
+      <div className='bar' />
+      <div className='gear-inner' />
+    </div>
   </div>
-);
+)
