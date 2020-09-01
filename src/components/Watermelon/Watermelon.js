b0658cee4f671d63a0078d30b9955f2a5b61d059
Tata Rozhdez <tanya.zhukova.95@gmail.com>
2020-09-01 23:35:33 +0300

    adding animation

--- src/components/Watermelon/Watermelon.js
+++ src/components/Watermelon/Watermelon.js
@@ -4,22 +4,12 @@ import './Watermelon.css'
 export const Watermelon = () => (
   <div className='watermelon grid-square block'>
     <div className='watermelon-img'>
-      <span className='seeds' />
-      <span className='seeds' />
-      <span className='seeds' />
-      <span className='seeds' />
-      <span className='seeds' />
-      <span className='seeds' />
-      <span className='seeds' />
-      <span className='seeds' />
-      <span className='seeds' />
-      <span className='seeds' />
-      <span className='seeds' />
-      <span className='seeds' />
-      <span className='seeds' />
-      <span className='seeds' />
-      <span className='seeds' />
-      <span className='seeds' />
+      <span className='seeds seed1' />
+      <span className='seeds seed2' />
+      <span className='seeds seed3' />
+      <span className='seeds seed4' />
+      <span className='seeds seed5' />
+      <span className='seeds seed6' />
     </div>
   </div>
 )
